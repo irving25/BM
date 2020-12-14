87280db62fb2c9b772f47059b95205431d858b36
iperez <iperez@EC2AMAZ-DJL71OR.firstmajestic.local>
2020-12-14 17:16:19 -0600

    Create_vehicle

--- Scripts/Catalogos/Transportistas/Vehiculos/Captura/Create_vehicle/Script1603568222940.groovy
+++ Scripts/Catalogos/Transportistas/Vehiculos/Captura/Create_vehicle/Script1603568222940.groovy
@@ -26,7 +26,7 @@ WebUI.click(findTestObject('Object Repository/Catalogos/Transportistas/Vehiculo/
 
 WebUI.click(findTestObject('Object Repository/Catalogos/Transportistas/Vehiculo/button_Captura'))
 
-for (def i = 1; i <= findTestData('Catalogos/Transportistas/Vehiculos').setProperty('', ''); i++) {
+for (def i = 1; i <= findTestData('Catalogos/Transportistas/Vehiculos').getRowNumbers(); i++) {
    
 WebUI.selectOptionByLabel(findTestObject('Object Repository/Catalogos/Transportistas/Vehiculo/select_Seleccionar TransportistaUshipAusenc_0612e0'), 
         findTestData('Data Files/Catalogos/Transportistas/Vehiculos').getValue(1, i), false, FailureHandling.CONTINUE_ON_FAILURE)
